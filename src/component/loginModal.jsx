d509d3f495a503a83b026cd3d76d66cec16c6aa3
LeeYena <leeyena@LeeYenas-MacBook-Air.local>
2024-07-07 18:33:57 +0900

    Feat: 로그인과 회원가입 모달창 제어 app.js에 추가

---
+++ src/component/loginModal.jsx
@@ -0,0 +1,17 @@
+import '../style/loginModal.css';
+
+export function LoginModal() {
+    return (
+        <div className="login-modal">
+            <p>login modal</p>
+        </div>
+    )
+}
+
+export function RegisterModal() {
+    return (
+        <div className="login-modal">
+            <p>register modal</p>
+        </div>
+    )
+}
