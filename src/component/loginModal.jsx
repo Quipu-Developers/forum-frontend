20dfcf983d4fa7c79576dcc919587a0a23056462
LeeYena <leeyena@LeeYenas-MacBook-Air.local>
2024-07-11 19:39:59 +0900

    Style: 로그인 모달창 요소 추가

--- src/component/loginModal.jsx
+++ src/component/loginModal.jsx
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
 import '../style/loginModal.css';
 
 export function LoginModal() {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className="login-modal">
-            <p>login modal</p>
+            <h2>QUIPU</h2>
+            <input placeholder='학번이름 ex) 1234567890퀴푸' onChange={(event) => setId(event.target.value)}></input>
+            <input placeholder='비밀번호' onChange={(event) => setPassword(event.target.value)}></input>
+            <button>로그인</button>
+            <p>비밀번호 찾기</p>
         </div>
     )
 }
