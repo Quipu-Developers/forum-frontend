9bda5ce9a9c031340f7429c052dd3b4a89e6cf7a
juhyun <wngus5861@naver.com>
2024-07-21 19:09:34 +0900

    list-페이지네이션 뼈대 구현

---
+++ src/component/pagination.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+import './pagination.css';
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+const Pagination = ({ pageCount, onPageChange, currentPage }) => {
+  return (
+    <ReactPaginate
+      previousLabel={<FiChevronLeft />}
+      nextLabel={<FiChevronRight />}
+      pageCount={pageCount}
+      onPageChange={onPageChange}
+      containerClassName={"pagination"}
+      pageLinkClassName={"pagination__link"}
+      activeLinkClassName={"pagination__link__active"}
+    />
+  );
+};
+
+export default Pagination;
